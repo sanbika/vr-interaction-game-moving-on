a3c078cff9cbc3c726be88175d821a2c7bbaeab9
MingMei Lyu <sanbika2719@gmail.com>
2020-11-10 22:06:34 +0800

    cube movement script rename to cube behavior and add cube property, all existing layer merge into cube, saber script read and justify color

--- Assets/Scripts/CubeBehavior.cs
+++ Assets/Scripts/CubeBehavior.cs
@@ -1,9 +1,14 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class CubeMovement : MonoBehaviour
+public class CubeBehavior : MonoBehaviour
 {
+    public string color;
+    public string directionType;
+    public Boolean isBomb;
+
     // Start is called before the first frame update
     void Start()
     {
--- Assets/Scripts/Saber.cs
+++ Assets/Scripts/Saber.cs
@@ -1,11 +1,13 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System.Runtime.InteropServices;
 using UnityEngine;
 
 public class Saber : MonoBehaviour
 {
-    //the layer of the cube that is going to be cut
+  
     public LayerMask layer;
+    public string saberColor;
     private Vector3 previousPosition;
     // Start is called before the first frame update
     void Start()
@@ -20,20 +22,51 @@ public class Saber : MonoBehaviour
         Ray hitRay = new Ray(transform.position, transform.forward);
         //RaycastHit stores the info on the colliders hit(to find out which objects are intersected by the ray)
         RaycastHit hit;
-        //legnth of ray
+        //length of ray
         float distance = 1.1f;
 
         //(to be deleted!)see the ray in game to do adjustment
         //Debug.DrawRay(transform.position, transform.forward * distance);
 
-        var betableObjectMask = LayerMask.GetMask("bombCubes", "doubleDirCubes",
-                                                    "greenCubes", "grayCubes");
-        var bothColorMask = LayerMask.GetMask("greenCubes", "grayCubes", "doubleDirCubes");
+        if (Physics.Raycast(hitRay, out hit, distance, layer))
+        {
+            //check with bomb first, then color, then direction
+            var cube = hit.collider.gameObject.GetComponent<CubeBehavior>();
+            var cubeObj = hit.collider.gameObject;
+            //??????????????Difference between collider and transform?????
 
-        Debug.Log(betableObjectMask);
+            if (cube.isBomb)
+            {
+                //TODO points decrease
+                //TODO false effect
+            }
+            else
+            {
+                if (cube.color != this.saberColor)
+                {
+                    //TODO points decrease
+                    //TODO false effect
+                }
+                else
+                {
+                    if (cube.directionType == "no")
+                    {
+                        Destroy(cubeObj);
+                    }
 
-        if (Physics.Raycast(hitRay, out hit, distance, betableObjectMask))
+                    else if (cube.directionType == "single")
+                    {
+                        //to check if the cut direction right
+                        if (Vector3.Angle(transform.position - previousPosition, hit.transform.up) > 130)
+                        {
+                            Destroy(cubeObj);
+                        }
 
+                    }
+                }
+
+            }
+        }
         //below is the previous code
 /*        if (Physics.Raycast(hitRay, out hit, distance, layer))
         {
@@ -72,6 +105,26 @@ public class Saber : MonoBehaviour
  *     single dir - green/gray
  *     no dir - green/gray
  *     double dir - both
- * The first problem is to create appropriate layer for them:
+ * The first method is to create appropriate layer for them:
  *      1. color 
+ *      2. direction
+ * If I use color layer to distinguish them, there are 3 types:
+ *      1. green
+ *      2. gray
+ *      3. white(double dir)
+ *    all above should be able to interact with sabers, so they should form a layerMask(betable).
+ *    
+ *    But then there is a problem. Since each object only has one layer. Both single dir and no dir have the same layer.(e.g. green)
+ *    
+ *    If I need to distinguish them, I need to add a new attribute for all cubes called 'directionType'. It can be used to justify whether 
+ *    it is single dir or no dir or double dir.
+ *    
+ *    But this method has a drawback, it is complicated to understand, to bind with related type of saber, a layer should be set in Unity
+ *    on the saber, then the progress to create layerMask should involved in variable(layer). 
+ *    
+ * If I use direction layer, there are also 3 types:
+ *      1. no
+ *      2. single
+ *      3. double
+ * No need to transfer in layer Variable but need to add color attribute to cube, create betable layerMask, and check with color attribute
 */
