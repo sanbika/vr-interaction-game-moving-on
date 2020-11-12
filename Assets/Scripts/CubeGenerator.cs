6530089fc4fa4d8dc0070e166cde2a50ca010876
MingMei Lyu <sanbika2719@gmail.com>
2020-11-12 16:10:55 +0800

    create obstacles,props prefab & modify script(unfinished)

--- Assets/Scripts/CubeGenerator.cs
+++ Assets/Scripts/CubeGenerator.cs
@@ -8,9 +8,15 @@ using Random = UnityEngine.Random;
 
 public class CubeGenerator : MonoBehaviour
 {
-    public GameObject[] types;
-    //points where the cube will be instantiated
-    public Transform[] points;
+    //for betable direction cube
+    public GameObject[] dirCubeTypes;
+    public Transform[] dirCubePoints;
+    //for obstacles
+    public GameObject[] obstaclesTypes;
+    public Transform[] obstaclePoints;
+    //for props
+    public GameObject[] propsTypes;
+
     //the time interval to generate a cube(according to the bgm)
     public float beat;
     //count time to xontrol the update loop
@@ -26,12 +32,27 @@ public class CubeGenerator : MonoBehaviour
     {
         if(timer > beat)
         {
-            //randomly generate color and points
-            GameObject cube = Instantiate(types[Random.Range(0, 6)], points[Random.Range(0, 4)]);
-            //make sure that cube is generated at the set point
-            cube.transform.localPosition = Vector3.zero;
-            //change cube randomly in four directions, up/down/left/right
-            cube.transform.Rotate(transform.forward, 90 * Random.Range(0, 4));
+            var probability = Random.Range(0, 10);
+            if(probability < 6)
+            {
+                //randomly generate color and points
+                GameObject cube = Instantiate(dirCubeTypes[Random.Range(0, 6)], dirCubePoints[Random.Range(0, 4)]);
+                //make sure that cube is generated at the set point
+                cube.transform.localPosition = Vector3.zero;
+                //change cube randomly in four directions, up/down/left/right
+                cube.transform.Rotate(transform.forward, 90 * Random.Range(0, 4));
+            }
+            else if( 6<= probability && probability < 8)
+            {
+                GameObject obastacle = Instantiate(obstaclesTypes[Random.Range(0, 1)], obstaclePoints[Random.Range(0, 2)]);
+                obastacle.transform.localPosition = Vector3.zero;
+            }
+            else
+            {
+                GameObject prop = Instantiate(obstaclesTypes[Random.Range(0, 1)], dirCubePoints[Random.Range(0, 4)]);
+                prop.transform.localPosition = Vector3.zero;
+            }
+
             //update timer
             timer -= beat;
         }
